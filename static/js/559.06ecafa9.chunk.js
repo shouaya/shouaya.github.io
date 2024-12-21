4778c8fab1ae9daae5d2ba14f79342ee9ae31880
cc.zhuang <cc.zhuang@weget.jp>
2024-12-21 12:22:54 +0900

    change page

---
+++ static/js/559.06ecafa9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstock_ec=self.webpackChunkstock_ec||[]).push([[559],{559:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=559.06ecafa9.chunk.js.map
